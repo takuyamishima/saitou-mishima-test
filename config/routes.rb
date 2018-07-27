ec4b7196f36ddec7ade59da4f07ae25dcac7da1b
takuya <takuyamishima@gmail.com>
2018-07-27 12:37:07 +0000

    aa

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+
+  resources :blogs do
+    collection do
+      post :confirm
+    end
+  end
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
+end
